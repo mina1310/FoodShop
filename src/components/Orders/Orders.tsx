b5371ef25881c37a174dcbc5d95c1a2ec2252011
mina1310 <mina.kavianig@gmail.com>
2025-01-27 23:41:59 +0330

    style(Orders):add styles to Orders component

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../store";
+import classes from './orders.module.css'
 const Orders:React.FC=()=>{
    const{selectedFoods}=useSelector((state:RootState)=>state.food)
    console.log("Selected Foods in State:", selectedFoods); 
    const content='Your cart is empty, go pick a delicious meal!'
 return(
-    <div>
+    <div className={classes['orders']}>
     {selectedFoods.length > 0 ? (
-      <div>
+      <div className={classes['orders__item']}>
         {selectedFoods.map((item) => (
           <li key={item.id}>
             <div>
-              <img src={`http://localhost:3000/${item.image}`} alt={item.name} />
+              <img className={classes['orders__item-image']} src={`http://localhost:3000/${item.image}`} alt={item.name} />
               <div>name:{item.name}</div>
               <div>Quantity: {item.quantity}</div>
               <div>What’s special about this food?<br/>:{item.description}</div>
